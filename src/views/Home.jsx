c6db5e089153328e3e18a0048ae50bc45be4e047
harish.s2 <harish.s@circles.asia>
2024-11-19 01:37:09 +0800

    wip portfolio

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -79,7 +79,7 @@ const Home = () => {
             className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10"
 						rel="noreferrer"
 						style={{ width: 'fit-content' }}>
-              Resume
+              Resume 
 					</motion.a>
               </div>
             </div>
